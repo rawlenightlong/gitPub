7a1233a1800ddb15ed7ae27551b32018c2e508ce
Rawle Becerril <rawlesxm@gmail.com>
2022-11-11 23:20:35 -0500

    working show and index routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const drinks = require('./models/drinks')
+// console.log(drinks)
 
 
 
@@ -10,6 +12,24 @@ app.listen(3000, () => {
 
 // First GET Route
 
-app.get('/', (req, res) => {
-    res.send('Welcome To The Gitpub App!')
+app.get('/gitpub', (req, res) => {
+    res.send(`Welcome to GitPub! <a href="gitpub/drinks">Take a look at our menu</a>`)
+})
+
+
+// INDEX Route
+
+app.get('/gitpub/drinks', (req, res) => {
+    res.render('drinks_index.ejs', {
+        allDrinks: drinks
+    })
+})
+
+// SHOW Route
+
+app.get('/gitpub/drinks/:id', (req, res) => {
+    res.render('drinks_show.ejs', {
+        drink: drinks[req.params.id],
+        id: req.params.id
+    })
 })
